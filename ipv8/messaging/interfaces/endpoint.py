9c08a9cc4f4ed7927faa7ce2b1c0275c4e20c647
Martijn de Vos <mdmartijndevos@gmail.com>
2020-02-25 16:05:14 +0100

    Revert "Running code on TP"
    This reverts commit d1b83f2e3dc30bb1eb9a53c1bcd27b0303f3a9d4.

--- ipv8/messaging/interfaces/endpoint.py
+++ ipv8/messaging/interfaces/endpoint.py
@@ -2,7 +2,6 @@ import abc
 import logging
 import socket
 import struct
-from asyncio import get_event_loop
 
 try:
     # Especially on Android netifaces may fail.
@@ -42,7 +41,7 @@ class Endpoint(metaclass=abc.ABCMeta):
         Ensure that the listener is still loaded when delivering the packet later.
         """
         if self.is_open() and listener in self._listeners:
-            get_event_loop().run_in_executor(None, listener.on_packet, packet)
+            listener.on_packet(packet)
 
     def notify_listeners(self, packet):
         """
